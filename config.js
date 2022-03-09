7c9a7782e7bc18036a84bc8b0a7e03a5958d65c8
papayoda <dim800vt@gmail.com>
2022-03-09 12:49:57 +0200

    Fix:email url vol2

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ const CONFIG = {
       id: '2',
       name: 'Gmail',
       icon: 'mail',
-      link: 'https://mail.google.com/',
+      link: 'https://mail.google.com/r',
     },
     {
       id: '3',
